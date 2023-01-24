400eaf5a094ec59e023141dd85a5e495fa53f977
Fady Mondy <EngFadyMondy@gmail.com>
2023-01-24 11:08:58 +0200

    update laravel-sanctum

--- src/Interfaces/Auth.php
+++ src/Interfaces/Auth.php
@@ -2,65 +2,68 @@
 
 namespace io3x1\LaravelAuthBuilder\Interfaces;
 
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\RedirectResponse;
+
 interface Auth
 {
     /**
      * @param \Illuminate\Http\Request $request
-     * @return \Illuminate\Http\JsonResponse
+     * @return JsonResponse|RedirectResponse
      */
-    public function register(\Illuminate\Http\Request $request): \Illuminate\Http\JsonResponse;
+    public function register(\Illuminate\Http\Request $request): JsonResponse|RedirectResponse;
 
     /**
      * @param \Illuminate\Http\Request $request
-     * @return \Illuminate\Http\JsonResponse
+     * @return JsonResponse|RedirectResponse
      */
-    public function login(\Illuminate\Http\Request $request): \Illuminate\Http\JsonResponse;
+    public function login(\Illuminate\Http\Request $request): JsonResponse|RedirectResponse;
 
     /**
      * @param \Illuminate\Http\Request $request
-     * @return \Illuminate\Http\JsonResponse
+     * @return JsonResponse|RedirectResponse
      */
-    public function logout(\Illuminate\Http\Request $request): \Illuminate\Http\JsonResponse;
+    public function logout(\Illuminate\Http\Request $request): JsonResponse|RedirectResponse;
 
     /**
      * @param \Illuminate\Http\Request $request
-     * @return \Illuminate\Http\JsonResponse
+     * @return JsonResponse|RedirectResponse
      */
-    public function resend(\Illuminate\Http\Request $request): \Illuminate\Http\JsonResponse;
+    public function resend(\Illuminate\Http\Request $request): JsonResponse|RedirectResponse;
 
     /**
      * @param \Illuminate\Http\Request $request
-     * @return \Illuminate\Http\JsonResponse
+     * @return JsonResponse|RedirectResponse
      */
-    public function profile(\Illuminate\Http\Request $request): \Illuminate\Http\JsonResponse;
+    public function profile(\Illuminate\Http\Request $request): JsonResponse|RedirectResponse;
 
     /**
      * @param \Illuminate\Http\Request $request
-     * @return \Illuminate\Http\JsonResponse
+     * @return JsonResponse|RedirectResponse
      */
-    public function update(\Illuminate\Http\Request $request): \Illuminate\Http\JsonResponse;
+    public function update(\Illuminate\Http\Request $request): JsonResponse|RedirectResponse;
 
     /**
      * @param \Illuminate\Http\Request $request
-     * @return \Illuminate\Http\JsonResponse
+     * @return JsonResponse|RedirectResponse
      */
-    public function password(\Illuminate\Http\Request $request): \Illuminate\Http\JsonResponse;
+    public function password(\Illuminate\Http\Request $request): JsonResponse|RedirectResponse;
 
     /**
      * @param \Illuminate\Http\Request $request
-     * @return \Illuminate\Http\JsonResponse
+     * @return JsonResponse|RedirectResponse
      */
-    public function reset(\Illuminate\Http\Request $request): \Illuminate\Http\JsonResponse;
+    public function reset(\Illuminate\Http\Request $request): JsonResponse|RedirectResponse;
 
     /**
      * @param \Illuminate\Http\Request $request
-     * @return \Illuminate\Http\JsonResponse
+     * @return JsonResponse|RedirectResponse
      */
-    public function otp(\Illuminate\Http\Request $request): \Illuminate\Http\JsonResponse;
+    public function otp(\Illuminate\Http\Request $request): JsonResponse|RedirectResponse;
 
     /**
      * @param \Illuminate\Http\Request $request
-     * @return \Illuminate\Http\JsonResponse
+     * @return JsonResponse|RedirectResponse
      */
-    public function destroy(\Illuminate\Http\Request $request): \Illuminate\Http\JsonResponse;
+    public function destroy(\Illuminate\Http\Request $request): JsonResponse|RedirectResponse;
 }
